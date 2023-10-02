00e2ca1c3229548ad2ddb95284bd1ce745b18ff8
Kshitijkumar16 <85787820+Kshitijkumar16@users.noreply.github.com>
2023-10-02 21:58:54 +0530

    updates

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,41 +4,44 @@ import { FooterOverlay } from '../components';
 import { gericht, spoon } from '../assets';
 
 const Footer = () => (
-  <section className="padding-x" id="footer-section">
+  <section className="w-full flex items-center flex-col justify-between padding-x text-golden font-cormorant pt-24" id="footer-section">
     <FooterOverlay />
 
-    <div className="app__footer-links">
-      <div className="app__footer-links_contact">
-        <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+
+    <div className=" border-2 flex gap-10 items-start justify-betw">
+      <div className="w-1/3 flex flex-1 flex-col item-center border-2 justify-center h-full">
+        <div>
+          <h1 className="text-white">Contact Us</h1>
+          <p className="font-alt">Street A Bunglow, WallStreet Lane Right, Atlantis, NAM-0R</p>
+          <p className="font-alt">+0 123-423-1234</p>
+          <p className="font-alt">+0 123-423-1234</p>
+        </div>
       </div>
 
-      <div className="app__footer-links_logo">
+      <div className="w-1/3 flex flex-1 flex-col item-start justify-between">
         <img src={gericht} alt="footer_logo" />
-        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
-        <img src={spoon} className="spoon__img" style={{ marginTop: 15 }} />
-        <div className="app__footer-links_icons">
+        <p className="">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
+        <img src={spoon} className="mt-4" />
+        <div className="bg-white">
           <FiFacebook />
           <FiTwitter />
           <FiInstagram />
         </div>
       </div>
 
-      <div className="app__footer-links_work">
-        <h1 className="app__footer-headtext">Working Hours</h1>
-        <p className="p__opensans">Monday-Friday:</p>
-        <p className="p__opensans">08:00 am - 12:00 am</p>
-        <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">07:00 am - 11:00 pm</p>
+      <div className="w-1/3  border-2  ">
+        <div className='flex flex-col items-center'>
+          <h1 className="text-white">Working Hours</h1>
+          <p className="font-alt">Monday-Friday:</p>
+          <p className="font-alt">08:00 am - 12:00 am</p>
+          <p className="font-alt">Saturday-Sunday:</p>
+          <p className="font-alt">07:00 am - 11:00 pm</p></div>
       </div>
     </div>
 
-    <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+    <div className="">
+      <p className="font-alt">2021 Gericht. All Rights reserved.</p>
     </div>
-
   </section>
 );
 
